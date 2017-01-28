964c862c7f95481f0f756fb886eb30429cf46671
Priyanka <priyanka3007@gmail.com>
2017-01-28 10:53:42 -0800

    Added fnx

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'numbers#index'
 
+  lets do this
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
