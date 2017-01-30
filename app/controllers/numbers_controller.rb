cd0ace9de05f550bb3f58772aca8947ecf1ea81b
Priyanka <priyanka3007@gmail.com>
2017-01-30 13:38:34 -0800

    Need to work on elsif statement

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,4 +1,29 @@
 class NumbersController < ApplicationController
-  def index
-  end
+	def index
+  	session[:random] = rand(1..100) if !session[:random].present?
+	end
+
+	def create
+
+  		flash[:message] = "You got it" 
+  		flash[:status] = false
+
+  	    if params[:number].present?
+  	    	input_number = params[:number].to_i 
+
+  	    	if input_number < session[:random]
+  	    		flash[:message] = "Too Low"
+
+  	    	elsif input_number > session[:random]
+ 				flash[:messsage] = "Too High" 
+ 				
+ 			else
+ 				flash[:status] = true
+			end
+
+ 		else 
+ 			flash[:message] = "Type a number"
+ 		end
+ 		redirect_to action: "index"
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root 'numbers#index'
 
-  
+  post 'answer' => 'numbers#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
